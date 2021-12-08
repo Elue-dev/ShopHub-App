01084f814d3e50b61d6818e58e9af6901b7d32fa
Elue-dev <eluewisdom@gmail.com>
2021-12-08 15:24:35 +0100

    changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import React, { useEffect, useContext } from 'react'
 import { StoreContext } from '../context/StoreContext'
 import { SingleProduct } from './SingleProduct'
 import '../App.css'
+import gif from '../assets/loader3.gif'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { Pagination } from './Pagination';
 library.add(faSpinner);
+
 
 export const Home = () => {
     const {products, setProducts, currentPage, postsPerPage} = useContext(StoreContext)
@@ -30,7 +32,8 @@ export const Home = () => {
         <>
             {products.length === 0 ? (
               <div className='spinner'>
-                <FontAwesomeIcon icon='spinner' size='3x' spin />
+                {/* <FontAwesomeIcon icon='spinner' size='3x' spin /> */}
+                <img src={gif}></img>
               </div>
             ) : (
               <div className='movie-grid wrapper'>
